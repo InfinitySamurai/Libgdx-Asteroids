93cfd6d46043c8130faa9be5f57107f52673647d
Russell Long <russell._long@hotmail.com>
2018-02-14 11:47:26 +1100

    Initial project

---
+++ desktop/src/net/infinitycorp/asteroids/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.infinitycorp.asteroids.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.infinitycorp.asteroids.Asteroids;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Asteroids(), config);
+	}
+}
