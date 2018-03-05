98bd0d9a21af08f45228bc8ec9237d9030536765
Russell Long <russell._long@hotmail.com>
2018-03-05 16:22:25 +1100

    Added screen wrapping for ship

--- core/src/net/infinitycorp/asteroids/Ship.java
+++ core/src/net/infinitycorp/asteroids/Ship.java
@@ -1,5 +1,6 @@
 package net.infinitycorp.asteroids;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -15,15 +16,33 @@ public class Ship{
     Ship (){
         texture = new Texture("Ship.png");
         sprite = new Sprite(texture);
+        sprite.setOriginCenter();
         sprite.setPosition(250, 250);
         sprite.setRotation(90);
-        velx = 1;
-        vely = 1;
+        velx = -5;
+        vely = -5;
     }
 
     public void update(float delta){
-        sprite.setX(sprite.getX() + velx);
-        sprite.setY(sprite.getY() + vely);
+        sprite.translate(velx, vely);
+        float windowHeight = Gdx.graphics.getHeight();
+        float windowWidth = Gdx.graphics.getWidth();
+
+        float xpos = sprite.getX();
+        float ypos = sprite.getY();
+        if (xpos > windowWidth){
+            sprite.setX(5);
+        }
+        else if (xpos < 0){
+            sprite.setX(windowWidth - 5);
+        }
+
+        if (ypos > windowHeight){
+            sprite.setY(5);
+        }
+        else if(ypos < 0){
+            sprite.setY(windowHeight - 5);
+        }
     }
 
     public void render(SpriteBatch sb){
