8161fe4935472413474e8baab57ed5dead6cf261
Russell Long <russell._long@hotmail.com>
2018-03-05 14:45:30 +1100

    First changes

--- desktop/src/net/infinitycorp/asteroids/desktop/DesktopLauncher.java
+++ desktop/src/net/infinitycorp/asteroids/desktop/DesktopLauncher.java
@@ -6,7 +6,9 @@ import net.infinitycorp.asteroids.Asteroids;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Asteroids(), config);
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.width = 1024;
+		cfg.height = 768;
+		new LwjglApplication(new Asteroids(), cfg);
 	}
 }
