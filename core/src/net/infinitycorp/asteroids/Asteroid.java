2c755dda72a270c30f4fe6a8a63aa3bb5e0d386e
Russell Long <russell._long@hotmail.com>
2018-03-06 16:05:09 +1100

    Started adding skeleton for asteroids

--- core/src/net/infinitycorp/asteroids/Asteroid.java
+++ core/src/net/infinitycorp/asteroids/Asteroid.java
@@ -1,4 +1,16 @@
 package net.infinitycorp.asteroids;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 public class Asteroid {
+
+    private Movement movement;
+
+    private Texture texture;
+    private Sprite sprite;
+
+    Asteroid(float speed, float rotation){
+        movement = new Movement(speed, rotation);
+    }
 }
